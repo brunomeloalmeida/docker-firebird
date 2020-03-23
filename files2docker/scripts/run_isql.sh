5bddc152ed70f13683e2294f55ab2c371b7a2802
brunoalmeida <bruno_melo_almeida@hotmail.com>
2020-03-23 08:43:57 -0300

    Refatorado scripts

--- files2docker/scripts/run_isql.sh
+++ files2docker/scripts/run_isql.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
-$DATABASE_NAME=$1
-/usr/local/firebird/bin/isql -u sysdba -p $ISC_PASSWORD /firebird/data/$DATABASE_NAME
+
+COLOR_RED='\033[1;31m';
+COLOR_GREEN='\033[1;32m';
+COLOR_BLUE='\033[1;36m';
+COLOR_YELLOW='\033[1;49;33m';
+COLOR_DARK_GRAY='\033[1;90m';
+COLOR_CLEAN='\033[0m';
+
+print() {
+  echo
+  echo -e "${COLOR_YELLOW}$@${COLOR_CLEAN}"
+}
+
+example() {
+  echo -e "${COLOR_DARK_GRAY}=================================="
+  echo "How to use it:"
+  echo " run_isql.sh <DATABASE_NAME>"
+  echo
+  echo " example:"
+  echo "   sh /scripts/run_isql.sh my_database.fdb"
+  echo -e "==================================${COLOR_CLEAN}"
+}
+
+# Variaveis
+DATABASE_NAME=$1
+
+validateParams() {
+  if [ -z $DATABASE_NAME ]; then
+    echo -e "${COLOR_RED}Error: Invalid parameters"
+    example;
+    exit 1;
+  fi
+}
+validateParams
+
+/usr/local/firebird/bin/isql -user sysdba -pas $ISC_PASSWORD /firebird/data/$DATABASE_NAME
